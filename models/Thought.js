fac57e5cd01c3ffb13c1c26446aae26583739744
jpd61 <jpd@dewoodyoil.com>
2020-07-12 14:47:51 -0500

    reaction subdocument model setup

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
+
+const ReactionSchema = new Schema(
+    {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+    },
+    {
+    toJSON: {
+        getters: true
+    } 
+    }
+);
 
 const ThoughtSchema = new Schema(
     {
@@ -28,6 +56,10 @@ const ThoughtSchema = new Schema(
     id: false
     }
 )
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
