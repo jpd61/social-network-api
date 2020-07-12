4bdc200eefa3dd09e5af01cab6260d7f291c04f1
jpd61 <jpd@dewoodyoil.com>
2020-07-12 15:03:30 -0500

    setup controller layout and structure and api routes

---
+++ controllers/thought-controller.js
@@ -0,0 +1,15 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getAllThoughts(req,res) {},
+    getThoughtById({ params }, res) {},
+    createThought({ params, body}, res) {},
+    updateThought({ params, body }, res) {},
+    deleteThought({ params }, res) {},
+    addReaction({ params, body}, res) {},
+    deleteReaction({ params }, res) {}
+
+
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,14 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {},
+    getUserById({ params}, res) {},
+    createUser({ body }, res) {},
+    updateUser({ params, body}, res) {},
+    deleteUser({ params }, res) {},
+    addFriend({ params }, res) {},
+    deleteFriend({ params }, res) {}
+
+};
+
+module.exports = userController; 
