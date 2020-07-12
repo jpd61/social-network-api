9cdfea6cf76d4625b40a246f2d0b3c0f98b0a663
jpd61 <jpd@dewoodyoil.com>
2020-07-12 14:37:42 -0500

    initial setup of User

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // use REGEX to validate email
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+})
+
+module.exports = User;
