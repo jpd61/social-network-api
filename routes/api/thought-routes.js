a74ab786b8d633535b202f254ec4d7ea3aa9815b
jpd61 <jpd@dewoodyoil.com>
2020-07-23 19:23:21 -0500

    api thought routes refactor

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,7 @@
+// Require express router
 const router = require('express').Router();
 
+// Set requirements (from thoughts-controller)
 const { 
     getAllThoughts, 
     getThoughtsById, 
@@ -12,25 +14,19 @@ const {
 } = require('../../controllers/thoughts-controller');
 
 // -- Directs to: /api/thoughts <GET>
-router.route('/')
-      .get(getAllThoughts);
+router.route('/').get(getAllThoughts);
 
 // -- Directs to: /api/thoughts/:id <GET, PUT, DELETE>
-router.route('/:id')
-      .get(getThoughtsById)
-      .put(updateThoughts)
-      .delete(deleteThoughts); 
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts); 
 
 // -- Directs to: /api/thoughts/:userId <POST>
-router.route('/:userId')    
-      .post(createThoughts);
+router.route('/:userId').post(createThoughts);
 
 // -- Directs to: /api/thoughts/:thoughtId/reactions <POST>
-router.route('/:thoughtId/reactions')
-      .post(addReaction);
+router.route('/:thoughtId/reactions').post(addReaction);
 
 // -- Directs to: /api/thoughts/:thoughtId/reactionId <DELETE>
-router.route('/:thoughtId/reactions/:reactionId')
-      .delete(deleteReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
+// Export module router
 module.exports = router;
